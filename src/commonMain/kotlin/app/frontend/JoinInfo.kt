9874b0a8ae3d79a008965217fb790164e3821073
jamowei <jan@rexster.de>
2021-01-04 18:32:28 +0100

    started join page

--- src/commonMain/kotlin/app/frontend/JoinInfo.kt
+++ src/commonMain/kotlin/app/frontend/JoinInfo.kt
@@ -1,10 +1,33 @@
 package app.frontend
 
+import dev.fritz2.identification.inspect
 import dev.fritz2.lenses.Lenses
+import dev.fritz2.validation.ValidationMessage
+import dev.fritz2.validation.Validator
+
+data class JoinValidationMessage(val id: String, val text: String) : ValidationMessage {
+    override fun isError(): Boolean = true
+}
 
 @Lenses
-data class JoinInfo(val room: String, val member: String) {
+data class JoinInfo(val room: String, val member: String, val roomCount: Int) {
     companion object {
-        val initial = JoinInfo("", "")
+        val initial = JoinInfo("", "", 0)
+        val validator = object : Validator<JoinInfo, JoinValidationMessage, Unit>() {
+            override fun validate(data: JoinInfo, metadata: Unit): List<JoinValidationMessage> {
+                val msgs = mutableListOf<JoinValidationMessage>()
+                val inspector = inspect(data)
+
+                val room = inspector.sub(L.JoinInfo.room)
+                if (room.data.isBlank())
+                    msgs.add(JoinValidationMessage(room.id, "Please provide a room name."))
+
+                val member = inspector.sub(L.JoinInfo.member)
+                if(member.data.isBlank())
+                    msgs.add(JoinValidationMessage(member.id, "Please provide a member name."))
+
+                return msgs
+            }
+        }
     }
 }
--- src/jsMain/kotlin/app/frontend/join.kt
+++ src/jsMain/kotlin/app/frontend/join.kt
@@ -1,20 +1,122 @@
 package app.frontend
 
+import dev.fritz2.binding.RootStore
+import dev.fritz2.binding.invoke
+import dev.fritz2.binding.watch
+import dev.fritz2.components.*
 import dev.fritz2.dom.html.RenderContext
+import dev.fritz2.remote.getBody
+import dev.fritz2.remote.http
+import dev.fritz2.styling.params.styled
+import kotlinx.browser.window
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 
+@ExperimentalCoroutinesApi
 fun RenderContext.joinPage() {
 
-    div("chat") {
-        div("chat-header clearfix") {
-            div("chat-about") {
-                div("chat-with") {
-                    +"Create a new chatroom"
+    val joinStore = object : RootStore<JoinInfo>(JoinInfo.initial) {
+        val remote = http("/rooms")
+
+        val join = handle {
+            console.info("join chat room: $it")
+            if(JoinInfo.validator.isValid(it, Unit)) {
+                window.open("/#room=${it.room.trim()}&member=${it.member.trim()}", target = "_blank")
+                JoinInfo.initial
+            } else it
+        }
+
+        val load = handle {
+            val count = remote.get().getBody().toInt()
+            it.copy(roomCount = count)
+        }
+
+        init { load() }
+    }
+
+    val roomStore = joinStore.sub(L.JoinInfo.room)
+    val memberStore = joinStore.sub(L.JoinInfo.member)
+
+    JoinInfo.validator.msgs.onEach { msgs ->
+        msgs.onEach { window.alert(it.text) }
+    }.watch()
+
+    lineUp {
+        items {
+            (::div.styled {
+                width { "750px" }
+                radii {
+                    topRight { normal }
+                    bottomRight { normal }
                 }
-                div("chat-num-messages") {
-                    +"Fill in user and room name"
+                color { "#434651" }
+            }) {
+                (::div.styled {
+                    padding { large }
+                    borders {
+                        bottom {
+                            width { "2px" }
+                            style { solid }
+                            color { "white" }
+                        }
+                    }
+                    background { color { "#f2f5f8" } }
+                }) {
+                    lineUp({
+                        alignItems { center }
+                    }) {
+                        items {
+                            icon({ size { giant } }) { fromTheme { fritz2 } }
+                            stackUp {
+                                spacing { none }
+                                items {
+                                    (::div.styled {
+                                        fontWeight { bold }
+                                        fontSize { large }
+                                    }) {
+                                        +"Join or create a chat room"
+                                    }
+                                    (::div.styled {
+                                        color { "#92959e" }
+                                    }) {
+                                        +"There are currently "
+                                        joinStore.data.map { it.roomCount }.asText()
+                                        +" chat rooms."
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                (::div.styled {
+                    padding { larger }
+                }) {
+                    formControl {
+                        inputField(id = roomStore.id, store = roomStore) {
+                            placeholder("Chat room name")
+                        }
+                    }
+                    formControl {
+                        inputField(id = memberStore.id, store = memberStore) {
+                            placeholder("Your name")
+                        }
+                    }
+                    flexBox({
+                        direction { row }
+                        justifyContent { center }
+                    }) {
+                        box {
+                            clickButton({
+                            }) {
+                                text("Join")
+                                icon { fromTheme { message } }
+                            } handledBy joinStore.join
+                        }
+                    }
                 }
             }
         }
     }
-
 }
